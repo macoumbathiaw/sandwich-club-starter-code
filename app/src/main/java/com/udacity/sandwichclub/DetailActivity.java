54805aeb15fe14cd3f826a0213d3b417415b1474
mthiaw <macoumbathiaw00@gmail.com>
2018-05-23 15:24:40 -0700

    4th push

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,6 +3,7 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,13 +12,19 @@ import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
     //Declaring the Views values
-    private TextView alsoKnownAsTv, placeOfOriginTv, descriptionTv, ingredientsTv;
+    private TextView alsoKnownAsTv, placeOfOriginTv, descriptionTv, ingredientsTv, alsoKnownAsVisibilityTv, placeOfOriginVisibilityTv, ingredientsVisibilityTv, descriptionVisibilityTv ;
     private ImageView sandwichPitcureIv;
 
 
@@ -32,7 +39,9 @@ public class DetailActivity extends AppCompatActivity {
         placeOfOriginTv = findViewById(R.id.origin_tv);
         descriptionTv = findViewById(R.id.description_tv);
         ingredientsTv = findViewById(R.id.ingredients_tv);
-
+        alsoKnownAsVisibilityTv = findViewById(R.id.also_known_tv_visibility);
+        placeOfOriginVisibilityTv = findViewById(R.id.origin_tv_visibility);
+        ingredientsVisibilityTv = findViewById(R.id.ingredients_tv_visibility);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -49,6 +58,7 @@ public class DetailActivity extends AppCompatActivity {
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -70,7 +80,41 @@ public class DetailActivity extends AppCompatActivity {
 
     private void populateUI(Sandwich sandwich) {
 
-        String AlsoKnown, OriginOfSandwich, DescriptionOfSandwich, IngredientsOfSandwich;
+        //Let's get the values of the key alsoKnownAs, and append them into the TexView alsoKnownAsTv. If the key is empty, we hide
+        if (sandwich.getAlsoKnownAs() !=null && !sandwich.getAlsoKnownAs().isEmpty()){
+            for ( int i =0; i<sandwich.getAlsoKnownAs().size(); i++) {
+                alsoKnownAsTv.append(sandwich.getAlsoKnownAs().get(i) + "\n" );
+            }
+        } else {
+            alsoKnownAsTv.setVisibility( View.GONE );
+            alsoKnownAsVisibilityTv.setVisibility(View.GONE);
+        }
+
+        // Let's get the place of origin, and set them into the TexView placeOfOriginTv. If the key is empty, we hide it
+        if (sandwich.getPlaceOfOrigin() != null && !sandwich.getPlaceOfOrigin().isEmpty()) {
+            placeOfOriginTv.setText( sandwich.getPlaceOfOrigin());
+        } else {
+            placeOfOriginTv.setVisibility(View.GONE);
+            placeOfOriginVisibilityTv.setVisibility(View.GONE);
+        }
+
+        // Let's get the ingredients, and append them into the TexView ingredientsTv. If the key is empty, we hide
+
+        if (sandwich.getIngredients() !=null && !sandwich.getIngredients().isEmpty()){
+            for ( int index =0; index <sandwich.getIngredients().size(); index++) {
+                ingredientsTv.append(sandwich.getIngredients().get(index) + "\n"  );
+            }
+        } else {
+            ingredientsTv.setVisibility( View.GONE );
+            ingredientsVisibilityTv.setVisibility(View.GONE);
+
+        }
+
+        // Let's get the descriptionTv, and set it into the TexView ingredientsTv. All sandwiches have a description
+        descriptionTv.setText( sandwich.getDescription());
+
+
+
 
 
     }
